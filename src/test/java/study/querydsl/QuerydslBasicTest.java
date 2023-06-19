6712806a7c63e2d42f9b0a52dd3afa8f784b268e
bluepeng9 <bluepeng9@gmail.com>
2023-06-19 19:57:31 +0900

    test: Add example of where clause with params

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,17 +2,16 @@ package study.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
-import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -52,13 +51,26 @@ public class QuerydslBasicTest {
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
+
     @Test
     public void startQuerydsl() {
-        QMember m = new QMember("m");
-
-        Member findMember = queryFactory.select(m).from(m).where(m.username.eq("member1")).fetchOne();
+        Member findMember = queryFactory.select(member).from(member).where(member.username.eq("member1")).fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
-
     }
+
+    @Test
+    public void search() {
+        Member findMember = queryFactory.selectFrom(member).where(member.username.eq("member1").and(member.age.eq(10))).fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void searchAndParam() {
+        Member findMember = queryFactory.selectFrom(member).where(member.username.eq("member1"), (member.age.eq(10))).fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
 }
