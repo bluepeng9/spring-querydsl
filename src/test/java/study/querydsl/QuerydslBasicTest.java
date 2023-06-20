9987ee425a576759b8f4b0dad95c436a028239ef
bluepeng9 <bluepeng9@gmail.com>
2023-06-20 12:19:16 +0900

    test: Add example of paging

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -120,4 +120,13 @@ public class QuerydslBasicTest {
         assertThat(member6.getUsername()).isEqualTo("member6");
         assertThat(memberNull.getUsername()).isNull();
     }
+
+    @Test
+    public void paging1() {
+        List<Member> result = queryFactory.selectFrom(member).orderBy(member.username.desc()).offset(1).limit(2).fetch();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+
 }
